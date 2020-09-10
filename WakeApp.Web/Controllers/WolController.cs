eb76f9c06d0ea550e9f429f13c71e1c965f0ef97
nagorniy-ad <nagorniy.ad@gmail.com>
2020-09-10 15:26:14 +1000

    Added WolController

---
+++ WakeApp.Web/Controllers/WolController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WakeApp.Core;
+
+namespace WakeApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WolController : ControllerBase
+    {
+        private readonly IWolService _wolService;
+
+        public WolController(IWolService wolService)
+        {
+            _wolService = wolService ?? throw new ArgumentNullException(nameof(wolService));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> WakeUpAsync(WolData data)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            var endpoint = data.ToRemoteEndPoint();
+            try
+            {
+                await _wolService.WakeUpAsync(remoteEndPoint: endpoint);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return Problem(detail: e.Message);
+            }
+        }
+    }
+}
