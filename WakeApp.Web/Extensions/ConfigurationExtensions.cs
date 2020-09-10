b58ef0ebc84894b004e206fc63b7f6f86fa2d787
nagorniy-ad <nagorniy.ad@gmail.com>
2020-09-10 15:33:58 +1000

    Web application configured

---
+++ WakeApp.Web/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using WakeApp.Core;
+
+namespace WakeApp.Web.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static ILocalEndPoint GetLocalEndPoint(this IConfiguration configuration)
+        {
+            var localEndPointSection = configuration.GetSection("LocalEndPoint");
+            IPAddress ipAddress = null;
+            int? port = null;
+            if (string.IsNullOrWhiteSpace(localEndPointSection["IpAddress"]) == false && IPAddress.TryParse(localEndPointSection["IpAddress"], out IPAddress parsedIpAddress) == true)
+            {
+                ipAddress = parsedIpAddress;
+            }
+            if (string.IsNullOrWhiteSpace(localEndPointSection["Port"]) == false && int.TryParse(localEndPointSection["Port"], out int parsedPort) == true)
+            {
+                port = parsedPort;
+            }
+            return new LocalEndPoint(ipAddress, port);
+        }
+    }
+}
--- WakeApp.Web/Startup.cs
+++ WakeApp.Web/Startup.cs
@@ -1,39 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WakeApp.Core;
+using WakeApp.Services;
+using WakeApp.Web.Extensions;
 
 namespace WakeApp.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private ILocalEndPoint LocalEndPoint { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            LocalEndPoint = configuration.GetLocalEndPoint();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IWolService, WolService>();
+            services.AddTransient(e => LocalEndPoint);
+            services.AddControllers();
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
