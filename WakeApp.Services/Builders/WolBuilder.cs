3bfbf6d2cf6a2b1a2cc3390451558578ca27fab3
nagorniy-ad <nagorniy.ad@gmail.com>
2020-09-10 15:11:53 +1000

    Added WolService implementation

---
+++ WakeApp.Services/Builders/WolBuilder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace WakeApp.Services
+{
+    public class WolBuilder
+    {
+        private const int DEFAULT_PORT = 9;
+        private readonly UdpClient _udp;
+        private readonly PhysicalAddress _macAddress;
+        private IPAddress _ipAddress;
+        private IPAddress _subnetMask;
+        private int _port;
+
+        public WolBuilder(IPAddress localIpAddress, int localPort, PhysicalAddress remoteMacAddress)
+        {
+            if (localIpAddress == null)
+            {
+                throw new ArgumentNullException(nameof(localIpAddress));
+            }
+            if (localPort <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(localPort));
+            }
+
+            _udp = new UdpClient(localEP: new IPEndPoint(address: localIpAddress, port: localPort));
+            _macAddress = remoteMacAddress ?? throw new ArgumentNullException(nameof(remoteMacAddress));
+            _port = DEFAULT_PORT;
+        }
+
+        public static WolBuilder Create(IPAddress localIpAddress, int localPort, PhysicalAddress remoteMacAddress)
+        {
+            return new WolBuilder(localIpAddress, localPort, remoteMacAddress);
+        }
+
+        public WolBuilder SetIpAddress(IPAddress ipAddress)
+        {
+            _ipAddress = ipAddress ?? throw new ArgumentNullException(nameof(ipAddress));
+            return this;
+        }
+
+        public WolBuilder SetSubnetMask(IPAddress subnetMask)
+        {
+            _subnetMask = subnetMask ?? throw new ArgumentNullException(nameof(subnetMask));
+            return this;
+        }
+
+        public WolBuilder SetPort(int port)
+        {
+            _port = (port > 0) ? port : throw new ArgumentOutOfRangeException(nameof(port));
+            return this;
+        }
+
+        public async Task WakeUpAsync()
+        {
+            using (_udp)
+            {
+                var magicPacket = CreateMagicPacketContent();
+                var endPoint = (_ipAddress != null && _subnetMask != null) ?
+                    new IPEndPoint(address: GetBroadcastAddressByIpAddress(), port: _port) :
+                    new IPEndPoint(address: IPAddress.Broadcast, port: _port);
+                await _udp.SendAsync(datagram: magicPacket, bytes: magicPacket.Length, endPoint: endPoint);
+            }
+        }
+
+        #region private
+
+        private byte[] CreateMagicPacketContent()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                byte[] syncStream = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+                memoryStream.Write(buffer: syncStream, offset: 0, count: syncStream.Length);
+                byte[] macAddress = _macAddress.GetAddressBytes();
+                for (int i = 0; i < 16; i++)
+                {
+                    memoryStream.Write(buffer: macAddress, offset: 0, count: macAddress.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        private IPAddress GetBroadcastAddressByIpAddress()
+        {
+            var address = BitConverter.ToInt32(value: _ipAddress.GetAddressBytes(), startIndex: 0);
+            var subnetMask = BitConverter.ToInt32(value: _subnetMask.GetAddressBytes(), startIndex: 0);
+            var broadcastAddress = address | ~subnetMask;
+            return new IPAddress(address: BitConverter.GetBytes(broadcastAddress));
+        }
+
+        #endregion
+    }
+}
---
+++ WakeApp.Services/Services/WolService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using WakeApp.Core;
+
+namespace WakeApp.Services
+{
+    public class WolService : IWolService
+    {
+        private readonly ILocalEndPoint _localEndPoint;
+
+        public WolService(ILocalEndPoint localEndPoint)
+        {
+            _localEndPoint = localEndPoint ?? throw new ArgumentNullException(nameof(localEndPoint));
+        }
+
+        public async Task WakeUpAsync(IRemoteEndPoint remoteEndPoint)
+        {
+            if (remoteEndPoint == null)
+            {
+                throw new ArgumentNullException(nameof(remoteEndPoint));
+            }
+            if (remoteEndPoint.MacAddress == null)
+            {
+                throw new ArgumentException(
+                    message: "MAC-адрес удаленной конечной точки не задан.",
+                    paramName: nameof(remoteEndPoint));
+            }
+
+            if (_localEndPoint.IpAddress == null)
+            {
+                throw new InvalidOperationException("IP-адрес локальной конечной точки не задан.");
+            }
+            if (_localEndPoint.Port == null)
+            {
+                throw new InvalidOperationException("Порт локальной конечной точки не задан.");
+            }
+            if (_localEndPoint.Port <= 0)
+            {
+                throw new InvalidOperationException("Порт локальной конечной точки некорректен.");
+            }
+            if (remoteEndPoint.IpAddress != null && remoteEndPoint.SubnetMask != null)
+            {
+                await WolBuilder.Create(_localEndPoint.IpAddress, _localEndPoint.Port.Value, remoteEndPoint.MacAddress)
+                    .SetIpAddress(remoteEndPoint.IpAddress)
+                    .SetSubnetMask(remoteEndPoint.SubnetMask)
+                    .WakeUpAsync();
+            }
+            else
+            {
+                await WolBuilder.Create(_localEndPoint.IpAddress, _localEndPoint.Port.Value, remoteEndPoint.MacAddress)
+                    .WakeUpAsync();
+            }
+        }
+    }
+}
