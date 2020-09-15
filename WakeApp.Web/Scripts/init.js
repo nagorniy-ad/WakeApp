eb6af4ecf80f179f81e5ae3823e2fdd91e793818
nagorniy-ad <nagorniy.ad@gmail.com>
2020-09-15 12:13:26 +1000

    WolData form validation implemented

--- WakeApp.Web/Scripts/init.js
+++ WakeApp.Web/Scripts/init.js
@@ -1,27 +1,87 @@
-﻿document.addEventListener('DOMContentLoaded', initialize);
+﻿let sendPacketButton;
+let wolDataForm;
+let macAddressInput;
+let ipAddressInput;
+let subnetMaskInput;
+
+document.addEventListener('DOMContentLoaded', initialize);
 
 function initialize() {
-    let sendButton = document.getElementById('send');
-    if (sendButton) {
-        sendButton.addEventListener('click', async function (e) {
+    sendPacketButton = document.getElementById('sendPacketButton');
+    wolDataForm = document.getElementById('wolDataForm');
+    macAddressInput = document.getElementById('macAddressInput');
+    ipAddressInput = document.getElementById('ipAddressInput');
+    subnetMaskInput = document.getElementById('subnetMaskInput');
+    if (sendPacketButton) {
+        sendPacketButton.addEventListener('click', function (e) {
             e.preventDefault();
-            let wolData = {
-                macAddress: '74-D4-35-0F-03-DF',
-                ipAddress: '192.168.3.131',
-                subnetMask: '255.255.255.0'
-            };
-            let notifier = new Notifier();
-            let client = new WolClient();
-            try {
-                await client.wakeUp(wolData);
-                notifier.success('Пакет успешно отправлен.');
-            }
-            catch {
-                notifier.error('Не удалось отправить пакет.');
-            }
+            sendPacket();
         });
     }
+    if (macAddressInput) {
+        macAddressInput.addEventListener('input', setMacAddressValidationRules);
+    }
+    if (ipAddressInput) {
+        ipAddressInput.addEventListener('input', setIpAddressValidationRules);
+    }
+    if (subnetMaskInput) {
+        subnetMaskInput.addEventListener('input', setSubnetMaskValidationRules);
+    }
+}
+
+async function sendPacket() {
+    if (wolDataForm.checkValidity() === true) {
+        let wolData = {
+            macAddress: macAddressInput.value,
+            ipAddress: ipAddressInput.value,
+            subnetMask: subnetMaskInput.value
+        };
+        let notifier = new Notifier();
+        let client = new WolClient();
+        try {
+            await client.wakeUp(wolData);
+            notifier.success('Пакет успешно отправлен.');
+        }
+        catch {
+            notifier.error('Не удалось отправить пакет.');
+        }
+        wolDataForm.classList.remove('was-validated');
+    }
     else {
-        console.error('Элемент "send" не найден.');
+        wolDataForm.classList.add('was-validated');
+    }
+}
+
+function setMacAddressValidationRules() {
+    let invalidFeedback = macAddressInput.parentElement.querySelector('.invalid-feedback');
+    if (macAddressInput.value) {
+        invalidFeedback.textContent = 'MAC-адрес некорректен.';
+    }
+    else {
+        invalidFeedback.textContent = 'MAC-адрес не задан.';
+    }
+}
+
+function setIpAddressValidationRules() {
+    let invalidFeedback = ipAddressInput.parentElement.querySelector('.invalid-feedback');
+    if (ipAddressInput.value) {
+        invalidFeedback.textContent = 'IP-адрес некорректен.';
+        subnetMaskInput.required = true;
+    }
+    else {
+        invalidFeedback.textContent = 'IP-адрес не задан.';
+        subnetMaskInput.required = false;
+    }
+}
+
+function setSubnetMaskValidationRules() {
+    let invalidFeedback = subnetMaskInput.parentElement.querySelector('.invalid-feedback');
+    if (subnetMaskInput.value) {
+        invalidFeedback.textContent = 'Маска подсети некорректна.';
+        ipAddressInput.required = true;
+    }
+    else {
+        invalidFeedback.textContent = 'Маска подсети не задана.';
+        ipAddressInput.required = false;
     }
 }
